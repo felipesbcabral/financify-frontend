73f9c09760bd36d79fc845a74adbb6cf10760241
jhondutrati <jhondutrati@gmail.com>
2023-04-16 22:54:13 -0300

    Criação das paginas de cadastro e esqueci senha

---
+++ src/components/RegisterPage.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./RegisterPage.css"; // Importe o arquivo de estilos
+
+const CadastroUsuarios = () => {
+  const [nomeUsuario, setNomeUsuario] = useState("");
+  const [senha, setSenha] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aqui você pode adicionar a lógica para validar os dados do usuário e realizar o cadastro
+
+    // Exemplo de validação simples apenas para fins ilustrativos
+    if (nomeUsuario === "" || senha === "" || email === "") {
+      alert("Por favor, preencha todos os campos.");
+    } else {
+      // Realizar o cadastro do usuário
+      alert("Usuário cadastrado com sucesso!");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Cadastro de Usuários</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="nomeUsuario">Nome de Usuário:</label>
+          <input
+            type="text"
+            id="nomeUsuario"
+            value={nomeUsuario}
+            onChange={(e) => setNomeUsuario(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="senha">Senha:</label>
+          <input
+            type="password"
+            id="senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">E-mail:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="button-container">
+          <button type="submit" className="btn-cadastrar">
+            Cadastrar
+          </button>
+          <button type="button" className="btn-voltar">
+            Voltar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CadastroUsuarios;
