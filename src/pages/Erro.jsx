1ff88449ef05319bf8b05e36fbd48343f8bacfba
FelipeCabralz <felipesbcabral@gmail.com>
2023-06-26 23:53:08 -0300

    :art: Padroniza botoes e melhora cor da pagina de erro #76

--- src/pages/Erro.jsx
+++ src/pages/Erro.jsx
@@ -12,35 +12,35 @@ function ErrorPage() {
   }
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-indigo-500 bg-fixed bg-cover bg-bottom error-bg">
+    <div className="flex items-center justify-center min-h-screen bg-fixed bg-cover bg-bottom error-bg">
       <div className="container">
         <div className="row">
-          <div className="col-sm-8 offset-sm-2 text-gray-50 text-center -mt-52">
+          <div className="col-sm-8 offset-sm-2 text-red-600 text-center -mt-52">
             <div className="relative">
               <h1 className="relative text-9xl tracking-tighter-less text-shadow font-sans font-bold">
                 <span>4</span>
                 <span>0</span>
                 <span>4</span>
               </h1>
-              <span className="absolute top-0 -ml-12 text-gray-300 font-semibold">
+              <span className="absolute top-0 -ml-12 text-red-600 font-semibold">
                 Oops!
               </span>
             </div>
-            <h5 className="text-gray-300 font-semibold -mr-10 -mt-3">
+            <h5 className="text-red-600 font-semibold -mr-10 -mt-3">
               Page not found
             </h5>
-            <p className="text-gray-100 mt-2 mb-6">
+            <p className="text-red-600 mt-2 mb-6">
               We are sorry, but the page you requested was not found.
             </p>
             <div className="flex flex-col">
               <button
-                className="bg-green-400 px-5 py-3 text-sm shadow-sm font-medium tracking-wider text-gray-50 rounded-full hover:shadow-lg mb-4"
+                className="bg-primary-600 px-5 py-3 text-sm shadow-sm font-medium tracking-wider text-gray-50 rounded-full hover:shadow-lg mb-4"
                 onClick={handleGoBack}
               >
                 Vá para a página inicial
               </button>
               <button
-                className="bg-blue-400 px-5 py-3 text-sm shadow-sm font-medium tracking-wider text-gray-50 rounded-full hover:shadow-lg"
+                className="bg-primary-600 px-5 py-3 text-sm shadow-sm font-medium tracking-wider text-gray-50 rounded-full hover:shadow-lg"
                 onClick={handleLogin}
               >
                 Vá para o login
